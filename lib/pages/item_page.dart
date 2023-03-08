308129cac4062a75b2d67fb425a940cff898264b
Fara <115084566+KHALMATOV-FARRUKH@users.noreply.github.com>
2023-03-09 01:33:07 +0500

    Added dependence: flutter_rating_bar: ^4.0.1
    Created:
    item_bottom_nav_bar.dart

--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -1,10 +1,213 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:nike_shoe_app_ui/widgets/item_bottom_nav_bar.dart';
 
 class ItemPage extends StatelessWidget {
   const ItemPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Color(0xFFF5F9FD),
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color(0xFF475269).withOpacity(0.3),
+                              spreadRadius: 1,
+                              blurRadius: 5,
+                            ),
+                          ],
+                        ),
+                        child: Icon(
+                          Icons.arrow_back,
+                          size: 30,
+                          color: Color(0xFF475269),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF5F9FD),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0xFF475269).withOpacity(0.3),
+                            spreadRadius: 1,
+                            blurRadius: 5,
+                          ),
+                        ],
+                      ),
+                      child: Icon(
+                        Icons.favorite,
+                        size: 30,
+                        color: Colors.redAccent,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 15),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.45,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      height: 230,
+                      width: 230,
+                      margin: EdgeInsets.only(top: 20, right: 70),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF475269),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Image.asset(
+                      "assets/images/1.png",
+                      height: 350,
+                      width: 350,
+                      fit: BoxFit.contain,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.4,
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                decoration: BoxDecoration(
+                  color: Color(0xFFF5F9FD),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(35),
+                    topRight: Radius.circular(35),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0xFF475269).withOpacity(0.4),
+                      blurRadius: 10,
+                      spreadRadius: 1,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "New Nike Shoe",
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF475269),
+                          ),
+                        ),
+                        Text(
+                          "\$55",
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.redAccent,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 15),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: RatingBar.builder(
+                        initialRating: 4,
+                        minRating: 1,
+                        direction: Axis.horizontal,
+                        itemSize: 20,
+                        itemCount: 5,
+                        itemPadding: EdgeInsets.symmetric(horizontal: 4),
+                        itemBuilder: (context, _) => Icon(
+                          Icons.favorite,
+                          color: Colors.redAccent,
+                        ),
+                        onRatingUpdate: (index) {},
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "This is description of the shoe product. This is description of the shoe product. This is description of the shoe product. This is description of the shoe product.",
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: Color(0xFF475269),
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Text(
+                          "Size",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Color(0xFF475269),
+                              fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(width: 10),
+                        Row(
+                          children: [
+                            for (int i = 40; i < 45; i++)
+                              Container(
+                                height: 35,
+                                width: 35,
+                                alignment: Alignment.center,
+                                margin: EdgeInsets.symmetric(horizontal: 5),
+                                decoration: BoxDecoration(
+                                  color: Color(0xFFF5F9FD),
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Color(0xFF475269).withOpacity(0.3),
+                                      blurRadius: 5,
+                                      spreadRadius: 1,
+                                    ),
+                                  ],
+                                ),
+                                child: Text(
+                                  "$i",
+                                  style: TextStyle(
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.w500,
+                                    color: Color(0xFF475269),
+                                  ),
+                                ),
+                              ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: ItemBottomNavBar(),
+    );
   }
 }
---
+++ lib/widgets/item_bottom_nav_bar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ItemBottomNavBar extends StatelessWidget {
+  const ItemBottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xFFF5F9FD),
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [],
+      ),
+    );
+  }
+}
